7e44cb4f62d304bb7467b7a2ff0aec2a3ebdd6e6
Julian Cannon <cannonj55@gmail.com>
2021-01-02 00:30:18 -0500

    testing changed heroku script

--- job.js
+++ job.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 
-var j = schedule.scheduleJob('* * * * * Sat', function () {
+// var j = schedule.scheduleJob('* * * * * Sun', function () {
 
   User.find({}, function (err, docs) {
     // cron.schedule('* 0 4 * * Wed', () => {
@@ -149,7 +149,7 @@ var j = schedule.scheduleJob('* * * * * Sat', function () {
       console.log("Error, unable to grab transmit data", err);
     }
   });
-});
+// });
 
 
 app.listen(process.env.PORT || 3000, function () {
